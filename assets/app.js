ca55d66d22f0385d6d612de993dc83af7eca02cd
Michal Muszynski <m.muszy@gmail.com>
2018-03-23 20:41:41 +0100

    JS and styles

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,14 @@
-$(document).ready(function() {
-    $.ajaxSetup({ cache: false });
-    $("button").on("click", function(){
-        $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(json) {
-         $("#get-text").html(JSON.stringify(json.quote
-         ));
+$(document).ready(function () {
+    $.ajaxSetup({cache: false});
+    $("button").on("click", function () {
+        $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function (json) {
+            $("#get-text").html(JSON.stringify(json.quote
+            ));
             $("#author").html(JSON.stringify(json.author
             ));
 
-            })
-
-
-        });
+        })
+    });
 
 });
 
