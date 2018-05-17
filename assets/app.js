95a6d6b4fcbd555cc8e88e0952d6bbaf85714dc5
Michal Muszynski <m.muszy@gmail.com>
2018-05-17 18:05:47 +0200

    Js updated

--- assets/app.js
+++ assets/app.js
@@ -16,7 +16,8 @@ $(document).ready(function () {
         var width = 300;
         var top = window.innerHeight-height;
         var left = window.innerHeight-width;
-        window.open("https://twitter.com/intent/tweet?text= " + $(".get-text").text() + ' by ' + $(".author").text(), target="_blank", 'location=yes, height=300,width=600, top='+top+', left='+left+', status=yes');
+        window.open("https://twitter.com/intent/tweet?text= " + $(".get-text").text() + ' by ' + $(".author").text(),
+            target="_blank", 'location=yes, height=300,width=600, top='+top+', left='+left+', status=yes');
     })
 
 });
