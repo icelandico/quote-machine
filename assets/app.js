b725651c70dcc9f29195308f49185adfd626c4f5
Michal Muszynski <m.muszy@gmail.com>
2018-05-19 22:12:10 +0200

    jQuery code replaced by plain JavaScript

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,45 @@
-$(document).ready(function () {
-    var api = "https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json";
+const api = new XMLHttpRequest();
+var quoteApi = "https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json";
+// var height = 550;
+// var width = 300;
+// var top = window.innerHeight-height;
+// var left = window.innerHeight-width;
+var getQuote = document.querySelector('.quote');
+var tweet = document.querySelector('.tweet');
+var quoteIn = document.querySelector('.get-text');
+var quoteAuthor = document.querySelector('.author');
 
 
-    $.ajaxSetup({cache: false});
-    $(".quote").on("click", function () {
-        $.getJSON(api, function (json) {
-            var randomApi = Math.floor(Math.random()*json.length+1);
-            $(".get-text").html('"'+ json[randomApi].quoteText +'"');
-            $(".author").html(json[randomApi].quoteAuthor);
+function parse() {
+    quoteIn.classList.remove('animate');
+    quoteAuthor.classList.remove('animate');
+    api.open('GET', quoteApi, true);
+    api.onload = function () {
+        data = JSON.parse(this.response);
+        console.log(data);
+        insertData(data);
+    };
+    api.send();
+}
 
-                })
-    });
-    $(".tweet").on("click", function() {
-        var height = 550;
-        var width = 300;
-        var top = window.innerHeight-height;
-        var left = window.innerHeight-width;
-        window.open("https://twitter.com/intent/tweet?text= " + $(".get-text").text() + ' by ' + $(".author").text(),
-            target="_blank", 'location=yes, height=300,width=600, top='+top+', left='+left+', status=yes');
-    })
+function insertData(data) {
+    var randomQuote = Math.floor(Math.random()* data.length + 1);
+    quoteIn.classList.add('animate');
+    quoteAuthor.classList.add('animate');
+    quoteIn.innerHTML = "\"" + data[randomQuote].quoteText + "\"";
+    quoteAuthor.innerHTML = data[randomQuote].quoteAuthor;
+}
 
-});
+function tweetQuote() {
+    window.open("https://twitter.com/intent/tweet?text= " +  quoteIn.innerText + ' by ' + quoteAuthor.innerText,
+    target="_blank", 'location=yes, status=yes, resizable=yes,top=500,left=500,width=400,height=400');
+    }
+
+getQuote.addEventListener('click', parse);
+tweet.addEventListener('click', tweetQuote);
+
+
+
 
 
 
