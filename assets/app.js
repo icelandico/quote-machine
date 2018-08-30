b6a81bb4c9c816994a1290c3d07af68b2d6b2efd
Michal Muszynski <m.muszy@gmail.com>
2018-08-30 19:12:53 +0200

    Changed API url

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 const api = new XMLHttpRequest();
-const quoteApi = "https://raw.githubusercontent.com/4skinSkywalker/Database-Quotes-JSON/master/quotes.json";
+const quoteApi = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 let getQuote = document.querySelector('.quote');
 let tweet = document.querySelector('.tweet');
 let quoteIn = document.querySelector('.get-text');
@@ -17,11 +17,11 @@ function parse() {
 }
 
 function insertData(data) {
-    let randomQuote = Math.floor(Math.random()* data.length + 1);
+    let randomQuote = Math.floor(Math.random()* data.quotes.length + 1);
     quoteIn.classList.add('animate');
     quoteAuthor.classList.add('animate');
-    quoteIn.innerHTML = "\"" + data[randomQuote].quoteText + "\"";
-    quoteAuthor.innerHTML = data[randomQuote].quoteAuthor;
+    quoteIn.innerHTML = "\"" + data.quotes[randomQuote].quote + "\"";
+    quoteAuthor.innerHTML = data.quotes[randomQuote].author;
 }
 
 function tweetQuote() {
